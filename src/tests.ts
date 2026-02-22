dd4d7c46e152325a53317ff04436e938b0549972
iainfox <furghadmanar@gmail.com>
2026-02-22 02:34:14 -0600

    add test elements to selected channels list

--- src/tests.ts
+++ src/tests.ts
@@ -1,7 +1,7 @@
-let channel_length = 4000
+let channel_length = 40
 
 if (available_channels_list) {
-    for (let i = 0; i < channel_length; i++) {
+    for (let i = 1; i <= channel_length; i++) {
         const li = document.createElement('li');
         const input = document.createElement('input');
         input.type = "checkbox";
@@ -16,5 +16,24 @@ if (available_channels_list) {
         li.appendChild(label);
 
         available_channels_list.appendChild(li);
+    }
+}
+
+if (selected_channels_list) {
+    for (let i = 1; i <= channel_length; i++) {
+        const li = document.createElement('li');
+        const input = document.createElement('input');
+        input.type = "checkbox";
+        input.className = "channel";
+        input.id = String(i);
+
+        const label = document.createElement('label');
+        label.textContent = String(i);
+
+        li.appendChild(input);
+        li.appendChild(document.createTextNode(' '));
+        li.appendChild(label);
+
+        selected_channels_list.appendChild(li);
     }
 }
