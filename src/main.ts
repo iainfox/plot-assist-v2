6638874787c00fb037c7621713b94ec4918d4156
iainfox <furghadmanar@gmail.com>
2026-02-22 15:20:12 -0600

    add esc keybind to deselect list

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,18 @@
 // }
 
 import "./available_channels";
+import "./selection_utils";
 import "./selected_channels";
+import { select_all } from "./selection_utils";
+
+const available_channels_list = document.getElementById("available-channels");
+const selected_channels_list = document.getElementById("selected-channels");
+
+document.addEventListener("keydown", (e) => {
+    if ((e.key === "Escape" || e.key === "Esc") && available_channels_list && selected_channels_list) {
+        select_all(available_channels_list, true)
+        select_all(selected_channels_list, true)
+        available_channels_list.classList.remove("selected");
+        selected_channels_list.classList.remove("selected");
+    }
+});
