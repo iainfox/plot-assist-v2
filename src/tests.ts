8d54c47b0ecf4ff58b640d772087500b5435f8b1
iainfox <furghadmanar@gmail.com>
2026-02-21 22:17:00 -0600

    add tests

---
+++ src/tests.ts
@@ -0,0 +1,21 @@
+let channel_length = 4000
+
+if (available_channels_list) {
+    for (let i = 0; i < channel_length; i++) {
+        const li = document.createElement('li');
+        const input = document.createElement('input');
+        input.type = "checkbox";
+        input.className = "channel";
+        input.id = String(i);
+
+        const label = document.createElement('label');
+        label.setAttribute('for', String(i));
+        label.textContent = String(i);
+
+        li.appendChild(input);
+        li.appendChild(document.createTextNode(' '));
+        li.appendChild(label);
+
+        available_channels_list.appendChild(li);
+    }
+}
