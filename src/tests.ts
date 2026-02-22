f82f5600048d895b94533d933c3bddf24bd0adcd
iainfox <furghadmanar@gmail.com>
2026-02-22 15:14:15 -0600

    fix glitches resulting from module splitting

--- src/tests.ts
+++ src/tests.ts
@@ -1,4 +1,6 @@
 let channel_length = 40
+const selected_channels_list = document.getElementById("selected-channels");
+const available_channels_list = document.getElementById("available-channels");
 
 if (available_channels_list) {
     for (let i = 1; i <= channel_length; i++) {
