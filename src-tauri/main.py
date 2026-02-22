307bb4c9c3e04b1037b480d307de70d1db6f8274
iainfox <furghadmanar@gmail.com>
2026-02-22 14:43:44 -0600

    fix column names

--- src-tauri/main.py
+++ src-tauri/main.py
@@ -3,15 +3,14 @@ import pandas
 import numpy as np
 
 x = np.arange(0, 1000)
-y = x
 sin_col = np.sin(x / 100) * 400 + 400
 cos_col = np.cos(x / 100) * 400 + 400
 square_wave = 402 + 397 * ((-1) ** np.floor(x / 100))
 
 df = pandas.DataFrame({
-    'sin_col': sin_col,
-    'y': y,
-    'cos_col': cos_col,
+    'sin': sin_col,
+    'linear': x,
+    'cos': cos_col,
     'square_wave': square_wave
 })
 
