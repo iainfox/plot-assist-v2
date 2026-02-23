ec5e0d6a1d3869151ae7e4fdfc70869b03236edb
iainfox <furghadmanar@gmail.com>
2026-02-22 22:17:39 -0600

    remove comment from tauri setup

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,3 @@
-// import { invoke } from "@tauri-apps/api/core";
-
-// let greetInputEl: HTMLInputElement | null;
-// let greetMsgEl: HTMLElement | null;
-
-// async function greet() {
-//   if (greetMsgEl && greetInputEl) {
-//     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-//     greetMsgEl.textContent = await invoke("greet", {
-//       name: greetInputEl.value,
-//     });
-//   }
-// }
-
 import "./available_channels";
 import "./selection_utils";
 import "./selected_channels";
