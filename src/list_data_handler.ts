0fe7ef55f900f6bedefbe71b12c2c9ca54be5052
iainfox <furghadmanar@gmail.com>
2026-02-24 21:21:14 -0600

    add function to the movemnt buttons

--- src/list_data_handler.ts
+++ src/list_data_handler.ts
@@ -2,6 +2,18 @@ import { invoke } from "@tauri-apps/api/core";
 
 const available_channels_list = document.getElementById("available-channels");
 const available_channels_list_warning = document.getElementById("list-warning");
+const selected_channels_list = document.getElementById("selected-channels");
+
+const add_all = document.getElementById("add-all")
+const add_all_grouped = document.getElementById("add-all-grouped")
+const add_selected = document.getElementById("add-selected")
+const add_selected_grouped = document.getElementById("add-selected-grouped")
+const seperate_group = document.getElementById("seperate-group")
+const combine_groups = document.getElementById("combine-groups")
+const move_back = document.getElementById("move-back")
+const move_forward = document.getElementById("move-forward")
+const remove_selected = document.getElementById("remove-selected")
+const remove_all = document.getElementById("remove-all")
 
 function add_channel(list: HTMLElement, str: string) {
     const li = document.createElement('li');
@@ -19,6 +31,23 @@ function add_channel(list: HTMLElement, str: string) {
 
     list.appendChild(li);
     console.log(`Added channel: ${str}`)
+}
+
+function clear_list(list: HTMLElement) {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function update_selected(new_list: string[][]) {
+    if (!selected_channels_list) return
+
+    clear_list(selected_channels_list)
+    for (let group_number = 0; group_number < new_list.length; group_number++) {
+        new_list[group_number].forEach(str => {
+            add_channel(selected_channels_list, str + ` [${group_number}]`)
+        });
+    }
 }
 
 function get_available(): string[] | undefined {
@@ -58,3 +87,154 @@ const names = await invoke<string[]>("get_channels", {});
 if (names.length != 0 && available_channels_list) {
     data_added()
 }
+
+add_all?.addEventListener("click", async (e) => {
+    if (!available_channels_list) return
+
+    const new_list = await invoke<string[][]>(
+        "add_channels",
+        {
+            channelNames: Array.from(available_channels_list.children).map((el) => (el as HTMLElement).textContent?.trim() || ""),
+            grouped: false
+        }
+    );
+
+    update_selected(new_list)
+});
+
+add_all_grouped?.addEventListener("click", async (e) => {
+    if (!available_channels_list) return
+
+    const new_list = await invoke<string[][]>(
+        "add_channels",
+        {
+            channelNames: Array.from(available_channels_list.children).map((el) => (el as HTMLElement).textContent?.trim() || ""),
+            grouped: true
+        }
+    );
+
+    update_selected(new_list)
+})
+
+add_selected?.addEventListener("click", async (e) => {
+    if (!available_channels_list) return
+
+    const new_list = await invoke<string[][]>(
+        "add_channels",
+        {
+            channelNames: get_available(),
+            grouped: false
+        }
+    );
+
+    update_selected(new_list)
+})
+
+add_selected_grouped?.addEventListener("click", async (e) => {
+    if (!available_channels_list) return
+
+    const new_list = await invoke<string[][]>(
+        "add_channels",
+        {
+            channelNames: get_available(),
+            grouped: true
+        }
+    );
+
+    update_selected(new_list)
+})
+
+seperate_group?.addEventListener("click", async (e) => {
+    const new_list = await invoke<string[][]>(
+        "",
+        {
+            
+        }
+    );
+
+    update_selected(new_list)
+})
+
+combine_groups?.addEventListener("click", async (e) => {
+    let selected = get_selected()
+    if (!selected) return
+    if (!(selected.length > 1)) return
+
+    let group_a_idx = 0
+    let group_b_idx = 0
+    for (let i=1; i<selected.length; i++) {
+        if (selected[group_a_idx][1] != selected[i][1]) {
+            group_b_idx = i
+        }
+    }
+    if (group_a_idx == group_b_idx) return
+
+    const new_list = await invoke<string[][]>("combine", {
+        groupA: selected[group_a_idx][1],
+        groupB: selected[group_b_idx][1],
+    });
+    update_selected(new_list)
+})
+
+move_back?.addEventListener("click", async (e) => {
+    let selected = get_selected()
+    if (!selected) return
+    if (!(selected.length > 0)) return
+
+    let previously_moved_groups: number[] = [];
+    for (let i=0; i<selected.length; i++) {
+        if (previously_moved_groups.includes(selected[i][1])) continue;
+        previously_moved_groups.push(selected[i][1])
+
+        const new_list = await invoke<string[][]>("move_backward", {
+            groupIndex: selected[i][1]
+        });
+        update_selected(new_list)
+    }
+})
+
+move_forward?.addEventListener("click", async (e) => {
+    let selected = get_selected()
+    if (!selected) return
+    if (!(selected.length > 0)) return
+
+    let previously_moved_groups: number[] = [];
+    for (let i=selected.length-1; i>=0; i--) {
+        if (previously_moved_groups.includes(selected[i][1])) continue;
+        previously_moved_groups.push(selected[i][1])
+
+        const new_list = await invoke<string[][]>("move_forward", {
+            groupIndex: selected[i][1]
+        });
+        update_selected(new_list)
+    }
+});
+
+remove_selected?.addEventListener("click", async (e) => {
+    if (!available_channels_list) return
+
+    let selected = get_selected();
+    let groupIndices: number[] = [];
+    let channelNames: string[] = [];
+    if (selected) {
+        for (let i = 0; i < selected.length; i++) {
+            groupIndices.push(selected[i][1]);
+            channelNames.push(selected[i][0]);
+        }
+    }
+
+    const new_list = await invoke<string[][]>(
+        "remove_channels",
+        {
+            groupIndices: groupIndices,
+            channelNames: channelNames
+        }
+    );
+
+    update_selected(new_list)
+})
+
+remove_all?.addEventListener("click", async (e) => {
+    const new_list = await invoke<string[][]>("remove_all", {});
+    update_selected(new_list)
+})
