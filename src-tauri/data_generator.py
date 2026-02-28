54edab13cc97362396b0c27d9c80c842067b0de0
iainfox <furghadmanar@gmail.com>
2026-02-27 18:24:01 -0600

    update data gen

--- src-tauri/data_generator.py
+++ src-tauri/data_generator.py
@@ -4,8 +4,8 @@ import numpy as np
 scale = 10000
 
 x = np.arange(0, scale)
-sin_col = np.sin(x / scale) * scale + scale
-cos_col = np.cos(x / scale) * scale + scale
+sin_col = (np.sin(2 * np.pi * x / scale) * (scale / 2)) + (scale / 2)
+cos_col = (np.cos(2 * np.pi * x / scale) * (scale / 2)) + (scale / 2)
 square_wave = np.floor(np.sin(x))*(scale-5)+scale
 
 df = pd.DataFrame({
